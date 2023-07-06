8de5a1a97f029d73b1824a1a4c239f08cb78bab0
Tushar Choudhari <choudharit23@gmail.com>
2023-07-06 23:40:59 +0530

    Add table component and linting errors

--- dashboard/src/components/feature-flags/index.tsx
+++ dashboard/src/components/feature-flags/index.tsx
@@ -1,17 +1,34 @@
 import { Plus } from 'lucide-react'
 import { Button } from '../ui/button'
+import { Table, TableBody, TableCell, TableHead, TableRow } from '../ui/table'
 import CreateFeatureFlagDialog from './CreateFeatureFlagDialog'
+import { TableHeader } from '../ui/table'
 
 const FeatureFlags = () => {
   return (
     <div className="w-full flex flex-col max-w-7xl">
-      <div className="w-full flex justify-end">
-        <CreateFeatureFlagDialog>
-          <Button>
-            <Plus className="mr-2 h-4 w-4" />
-            <span>New Feature Flag</span>
-          </Button>
-        </CreateFeatureFlagDialog>
+      <div className="w-full flex flex-col gap-6">
+        <div className="w-full flex justify-end">
+          <CreateFeatureFlagDialog>
+            <Button size="sm">
+              <Plus className="mr-2 h-4 w-4" />
+              <span>New Feature Flag</span>
+            </Button>
+          </CreateFeatureFlagDialog>
+        </div>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead>Enabled</TableHead>
+            </TableRow>
+            <TableBody>
+              <TableRow>
+                <TableCell>Dark Mode</TableCell>
+              </TableRow>
+            </TableBody>
+          </TableHeader>
+        </Table>
       </div>
     </div>
   )
