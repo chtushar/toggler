eb7341b209bfbbd2be846e0ee79b31e1b9dce63a
Tushar Choudhari <choudharit23@gmail.com>
2023-10-17 18:10:28 +0530

    Add codemirror editor

--- dashboard/src/scenes/root.tsx
+++ dashboard/src/scenes/root.tsx
@@ -1,26 +1,10 @@
-import { useEffect } from 'react'
-import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 import Layout from '@/components/common/layout'
-import SidebarConfigProvider from '@/context/SidebarConfigProvider'
-import useUserOrganizations from '@/hooks/queries/useUserOrganizations'
 
 const Root = () => {
-  const location = useLocation()
-  const navigate = useNavigate()
-  const { data: userOrgs } = useUserOrganizations()
-
-  useEffect(() => {
-    if (location.pathname === '/') {
-      navigate(`/${userOrgs?.data[0].uuid}/`)
-    }
-  }, [location.pathname, navigate, userOrgs?.data])
-
   return (
-    <SidebarConfigProvider>
-      <Layout>
-        <Outlet />
-      </Layout>
-    </SidebarConfigProvider>
+    <Layout>
+      <div className="flex items-center flex-col gap-8">tushar</div>
+    </Layout>
   )
 }
 
