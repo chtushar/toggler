e3686382f77b7313946f97f42a39679ac5653a59
Tushar Choudhari <choudharit23@gmail.com>
2023-10-26 00:32:03 +0530

    Add handlers boiler plate

--- api/apikeys/handlers.go
+++ api/apikeys/handlers.go
@@ -1,1 +1,15 @@
 package apikeys
+
+import "github.com/labstack/echo/v4"
+
+func handleCreateAPIKey(c echo.Context) error {
+	return nil
+}
+
+func handleGetOrgAPIKeys(c echo.Context) error {
+	return nil
+}
+
+func handleDeleteAPIKey(c echo.Context) error {
+	return nil
+}
